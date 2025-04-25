662b9254da83244cf325bc5c9e23842249292dbe
DanielEJones <djonesthe21st@gmail.com>
2025-04-25 22:03:41 +0800

    implemented simple assignment in proper parser

--- app/logic/specifier.py
+++ app/logic/specifier.py
@@ -1,4 +1,140 @@
 from __future__ import annotations
+from typing import Tuple
+
+
+# Examples:
+#
+# style = bar, x = header1, y = header2
+# style = line, x = header2, y = [header3, header4]
+#
+
+# Grammar:
+#
+#        spec ::= declaration [',' declaration]*
+# declaration ::= name '=' (name | '[' name_list ']')
+#   name_list ::= name [',' name]+
+#        name ::= [a-zA-Z]+
+#
+
+
+class Token:
+    is_a: str
+    lex: str
+    pos: int
+
+    def __init__(self, is_a: str, lex: str, pos: int) -> None:
+        self.is_a, self.lex, self.pos = is_a, lex, pos
+
+    @staticmethod
+    def new_punctuation(value: str, pos: int) -> Token:
+        return Token(value, value, pos)
+
+    @staticmethod
+    def new_error(message: str, pos: int) -> Token:
+        return Token('error', message, pos)
+
+    @staticmethod
+    def new_name(value: str, pos: int) -> Token:
+        return Token('name', value, pos)
+
+
+class Tokenizer:
+
+    def __init__(self, source: str) -> None:
+        self._source = source
+        self._head, self._tail = 0, 0
+        self._punctuation = {',', '=', '[', ']'}
+        self._has_more = True
+
+
+    def _at(self, index: int) -> str:
+        return self._source[index]
+
+    def _current(self) -> str:
+        return self._at(self._head)
+
+    def _test(self, value) -> bool:
+        return self._source[self._head : self._head + len(value)] == value
+
+    def _match(self, value: str) -> bool:
+        if self._test(value):
+            self._head += len(value)
+            return True
+        return False
+
+    def _get_lexeme(self) -> str:
+        return self._source[self._tail : self._head]
+
+    def has_more(self) -> bool:
+        return self._has_more and self._head < len(self._source)
+
+    def next_token(self) -> Token:
+        # catches tail up to head so that we start reading new token
+        self._tail = self._head
+
+        # skips whitespace
+        while self._match(' '):
+            self._tail = self._head
+
+        # match single character tokens
+        for elem in self._punctuation:
+            if self._match(elem):
+                return Token.new_punctuation(elem, self._tail)
+
+        # matches names
+        if self._current().lower() in 'abcdefghijklmnopqrstuvwxyz':
+            while self.has_more() and self._current().lower() in 'abcdefghijklmnopqrstuvwxyz':
+                self._head += 1
+            return Token.new_name(self._get_lexeme(), self._tail)
+
+        # if we didn't match anything it's an error, set has_more to false
+        self._has_more = False
+        self._head += 1
+        return Token.new_error(f'didn\'t recognise this char: {self._get_lexeme()}', self._tail)
+
+
+class ParseError(Exception):
+    ...
+
+
+class Parser:
+
+    def __init__(self, tokens: Tokenizer) -> None:
+        self._tokens = tokens
+        self._current = Token.new_error('No Tokens!', 0)
+
+    def _expect(self, is_a: str, message: str) -> str:
+        if not self._current.is_a == is_a:
+            raise ParseError(message)
+        return self._current.lex
+
+    def parse(self) -> dict[str, str]:
+        self._current = self._tokens.next_token()
+
+        if not self._current.is_a == 'name':
+            raise Exception('expected a name')
+        key = self._current.lex
+
+        if not self._tokens.has_more():
+            raise Exception('statement is incomplete')
+        self._current = self._tokens.next_token()
+
+        if not self._current.is_a == '=':
+            raise Exception('statement requires an assignment')
+        _ = self._current.lex
+
+        if not self._tokens.has_more():
+            raise Exception('statement is incomplete')
+        self._current = self._tokens.next_token()
+
+        if not self._current.is_a == 'name':
+            raise Exception('rhs of assignment requires a value')
+        value = self._current.lex
+
+        if self._tokens.has_more():
+            raise Exception('expected end of statement')
+
+        return {key: value}
 
 
 class Specifier:
--- tests/test_specifier.py
+++ tests/test_specifier.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
 
-from app.logic.specifier import Specifier
+from app.logic.specifier import Parser, Specifier, Tokenizer
 
 
 class Test(TestCase):
+
+    # Specifier Tests
 
     def test_given_headers(self) -> None:
         result = Specifier.from_string('x=x,y=y')
@@ -20,4 +22,74 @@ class Test(TestCase):
     def test_given_no_headers(self) -> None:
         with self.assertRaises(ValueError):
             Specifier.from_string('')
+
+    # Tokenizer tests
+
+    def test_test_true(self) -> None:
+        tk = Tokenizer('hello, world!')
+        self.assertTrue(tk._test('hello,'))
+
+    def test_test_false(self) -> None:
+        tk = Tokenizer('hello, world!')
+        self.assertFalse(tk._test('bello,'))
+
+    def test_test_in_middle(self) -> None:
+        tk = Tokenizer('hello, world!')
+        tk._head += 3
+        self.assertTrue(tk._test('lo, wo'))
+
+    def test_match_true(self) -> None:
+        tk = Tokenizer('hello, world!')
+        self.assertTrue(tk._match('hello,'))
+        self.assertEqual(6, tk._head)
+
+    def test_match_false(self) -> None:
+        tk = Tokenizer('hello, world!')
+        self.assertFalse(tk._match('bello,'))
+        self.assertEqual(0, tk._head)
+
+    def test_tokenizer_stops_with_error(self) -> None:
+        tk = Tokenizer('hello = [world, exclamationmark]>')
+
+        res = []
+        while tk.has_more():
+            res.append(tk.next_token())
+
+        self.assertEqual(
+            ['hello', '=', '[', 'world', ',', 'exclamationmark', ']', 'didn\'t recognise this char: >'],
+            [elem.lex for elem in res]
+
+        )
+
+    def test_tokenizer_stops_itself_simple(self) -> None:
+        tk = Tokenizer('hello = world')
+
+        res = []
+        while tk.has_more():
+            res.append(tk.next_token())
+
+        self.assertEqual(
+            ['hello', '=', 'world'],
+            [elem.lex for elem in res]
+
+        )
+
+    def test_tokenizer_stops_itself(self) -> None:
+        tk = Tokenizer('hello = [world, exclamationmark]')
+
+        res = []
+        while tk.has_more():
+            res.append(tk.next_token())
+
+        self.assertEqual(
+            ['hello', '=', '[', 'world', ',', 'exclamationmark', ']'],
+            [elem.lex for elem in res]
+        )
+
+    # Parser tests
+
+    def test_parser(self) -> None:
+        tk = Tokenizer('hello = world')
+        ps = Parser(tk).parse()
+        self.assertEqual({'hello': 'world'}, ps)
 
