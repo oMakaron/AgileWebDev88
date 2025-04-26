2234f619e1ce8f2de7748e00b0b12da1b283f7f6
William Vetter <william.joseph.vetter@gmail.com>
2025-04-26 17:50:02 +0800

    Setup Tailwindcss properly and start dashboard ui

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
+# Tell Flask where to find the static folder
+app = Flask(
+    __name__,
+    static_folder='../static',         # static is outside /app
+    template_folder='templates'        # templates are inside /app
+)
 
 # TODO: Make this not absolutely suck
 app.secret_key = 'very-secret'
@@ -8,5 +13,4 @@ app.secret_key = 'very-secret'
 from app import routes
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
