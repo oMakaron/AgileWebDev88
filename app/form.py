913fdabda66ea8cf04c7e570eb8ed806b4437b89
yuki-kii <23810522@student.uwa.edu.au>
2025-05-03 20:16:42 +0800

    Fix some small mistake

--- app/form.py
+++ app/form.py
@@ -5,17 +5,14 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 class SignupForm(FlaskForm):
     name = StringField('Full Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    
     password = PasswordField('Password', validators=[
         DataRequired(),
-        Length(min=8, message='Password too short.')
+        Length(min=8, message='Password too short. Minimum 8 characters.')
     ])
-    
     confirm = PasswordField('Confirm Password', validators=[
         DataRequired(),
         EqualTo('password', message='Passwords must match.')
     ])
-    
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
