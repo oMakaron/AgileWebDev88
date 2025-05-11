b8d9d1aa7bb2037a71a84d595a2e971b5068a8b3
DanielEJones <djonesthe21st@gmail.com>
2025-05-11 15:40:14 +0800

    implemented file apis

--- app/api/files.py
+++ app/api/files.py
@@ -1,25 +1,120 @@
-from flask import Blueprint, Response
+from os import abort, path, remove
+
+from flask import Blueprint, Response, abort, jsonify, request
+
+from ..models import File
+from ..extensions import db
 
 
 files = Blueprint('files', __name__)
 
 
+CURRENT_USER_ID = 1
+UPLOADS_FOLDER = path.join("app", "uploads")
+
+
 @files.route('/', methods=["GET"])
 def get_all_files() -> Response:
-    ...
+    files = File.query.filter_by(owner_id=CURRENT_USER_ID).all()
+    return jsonify([{'id': file.id, 'name': file.name} for file in files])
 
 
 @files.route('/', methods=["POST"])
 def make_new_file() -> Response:
-    ...
+    file = request.files.get('file')
+    name = request.form.get('name')
+
+    if not (file and name):
+        abort(400, desctiption="Missing required fiels.")
+
+    try:
+        new_file = File(name=name, owner_id=CURRENT_USER_ID) # type: ignore
+        db.session.add(new_file)
+        db.session.commit()
+
+        file_name = f"{new_file.id}.csv"
+        file_path = path.join(UPLOADS_FOLDER, file_name)
+
+        file.save(file_path)
+
+        response = jsonify({'id': new_file.id})
+        response.status_code = 201
+
+        return response
+
+    except:
+        db.session.rollback()
+        response = jsonify({'error': 'Internal server error.'})
+        response.status_code = 500
+        return response
 
 
 @files.route('/<int:file_id>/', methods=["GET"])
 def get_file(file_id: int) -> Response:
-    ...
+    file = File.query.get_or_404(file_id)
+
+    file_path = path.join(UPLOADS_FOLDER, f"{file_id}.csv")
+    if not path.exists(file_path):
+        response = jsonify({'error': 'File content is missing.'})
+        response.status_code = 404
+        return response
+
+    try:
+        with open(file_path, "r") as file:
+            content = file.read()
+        return Response(content, mimetype='text/csv')
+
+    except:
+        response = jsonify({'error': 'Internal server error.'})
+        response.status_code = 500
+        return response
 
 
 @files.route('/<int:file_id>/', methods=["PUT"])
 def edit_file(file_id: int) -> Response:
-    ...
+    file = File.query.get_or_404(file_id)
+
+    new_name = request.form.get('name')
+    new_file = request.files.get('file')
+
+    try:
+        if new_name:
+            file.name = new_name
+        if new_file:
+            file_path = path.join(UPLOADS_FOLDER, f"{file_id}.csv")
+            new_file.save(file_path)
+
+        db.session.commit()
+        return jsonify({'message': 'File updated successfully.'})
+
+    except:
+        db.session.rollback()
+
+        response = jsonify({'error': 'Internal server error.'})
+        response.status_code = 500
+        return response
+
+
+
+@files.route('/<int:file_id>/', methods=["DELETE"])
+def delete_file(file_id: int) -> Response:
+    file = File.query.get_or_404(file_id)
+
+    file_path = path.join(UPLOADS_FOLDER, f"{file_id}.csv")
+
+    try:
+        if path.exists(file_path):
+            remove(file_path)
+
+        db.session.delete(file)
+        db.session.commit()
+
+        return jsonify({'message': 'File deleted successfully.'})
+
+    except:
+        db.session.rollback()
+
+        response = jsonify({'error': 'Internal server error.'})
+        response.status_code = 500
+        return response
 
