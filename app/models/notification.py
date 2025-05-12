5aadad19e3de80f6ee97b709648dade448b9ea3c
oMakaron <nickalexander8257@gmail.com>
2025-05-13 06:01:53 +0800

    Added receiver column to notification

--- app/models/notification.py
+++ app/models/notification.py
@@ -3,6 +3,7 @@ from .base import Base
 
 class Notifications(Base):
     __tablename__ = 'notifications'
+    receiver = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     chart_share = db.Column(db.Integer, db.ForeignKey('shared_charts.id'), nullable=True)
     file_share = db.Column(db.Integer, db.ForeignKey('shared_files.id'), nullable=True)
     follower = db.Column(db.Integer, db.ForeignKey('follows.id'), nullable=True)
