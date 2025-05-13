033fe30cef68a6cdba677319b0edf3b494a0a199
oMakaron <nickalexander8257@gmail.com>
2025-05-13 08:50:09 +0800

    minor import addition in the models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,4 +3,4 @@ from .file import  File
 from .chart import Chart
 from .associations import SharedFile, SharedChart
 from .follow import Follows
-from .notification import Notifications
+from .notification import Notification
