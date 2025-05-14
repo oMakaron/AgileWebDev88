8a76a583b8b8bd5b21acecc19a44443abf193fb3
oMakaron <nickalexander8257@gmail.com>
2025-05-14 22:47:57 +0800

    typo fix on friends args constraint

--- app/models/friend.py
+++ app/models/friend.py
@@ -8,4 +8,4 @@ class Friend(Base):
     friend_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     is_friend = db.Column(db.Boolean, nullable=False, default=False)
     # prevent add one friend twice
-    __table_args__ = (db.UniqueConstraint('user_id', 'friend_id', name='unique_friendship'),)
+    __table_args__ = (db.UniqueConstraint('user_id', 'friend_id', name='unique_friendship'))
