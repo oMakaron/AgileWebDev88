4269f3bb68feb28950648f8a80a313bf24f21979
oMakaron <nickalexander8257@gmail.com>
2025-05-15 14:24:31 +0800

    adjust the notifications table

--- app/models/notification.py
+++ app/models/notification.py
@@ -1,7 +1,6 @@
 # app/models/notification.py
 from ..extensions import db
 from .base import Base
-from datetime import datetime
 
 
 class Notification(Base):
@@ -9,6 +8,4 @@ class Notification(Base):
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     message = db.Column(db.String(255), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     is_read = db.Column(db.Boolean, default=False)
-
