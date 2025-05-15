1c5cf0fa285134d1afd5df6c0608e8794826523f
DanielEJones <djonesthe21st@gmail.com>
2025-05-15 20:58:49 +0800

    reimplemented missing features

---
+++ app/models/friend.py
@@ -0,0 +1,9 @@
+from app.extensions import db
+
+class Friend(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    # prevent add one friend twice
+    __table_args__ = (db.UniqueConstraint('user_id', 'friend_id', name='unique_friendship'),)
+
