477f69fa5bed6778c8c19e4ea4b587ce1433c2ed
oMakaron <nickalexander8257@gmail.com>
2025-05-16 10:20:06 +0800

    notifications API adjustment for shared data

--- app/api/notifications.py
+++ app/api/notifications.py
@@ -1,54 +1,28 @@
 from flask import Blueprint, Response, jsonify, request, url_for
 from sqlalchemy import event, insert, delete
 
-from ..models import Notification, SharedChart, SharedFile, User, Chart, File, Friend
+from ..models import Notification, SharedData, User, Chart, Friend
 from .utils import require_login, get_user
 from ..extensions import db
 
 notifications = Blueprint('notifications', __name__, url_prefix='/notifications')
 
-@event.listens_for(SharedChart, 'after_update')
+@event.listens_for(SharedData, 'after_update')
 def chart_notif(mapper, connection, target):
     notif = Notification.__table__
     chart = Chart.query.get(target.chart_id)
-    sender = User.query.get(get_user())
+    sender = User.query.get(target.shared_by_user_id)
     connection.execute(
-        notif.insert().values(user_id=target.user_id,
+        notif.insert().values(user_id=target.shared_with_user_id,
                               message=f'{sender.fullname} have shared you a chart, {chart.name}',
                               href= url_for('routes.dashboard'))
     )
 
-@event.listens_for(SharedChart, 'before_delete')
+@event.listens_for(SharedData, 'before_delete')
 def chart_delete(mapper, connection, target):
     notif = Notification.__table__
     chart = Chart.query.get(target.chart_id)
     target_notif = Notification.query.filter(Notification.message.contains(chart.name)).first()
-    try:
-        connection.execute(
-            notif.delete().where(notif.c.id == target_notif.id)
-        )
-    except Exception:
-        pass
-
-@event.listens_for(SharedFile, 'after_update')
-def file_notif(mapper, connection, target):
-    notif = Notification.__table__
-    file = File.query.get(target.file_id)
-    sender = User.query.get(get_user())
-    try:
-        connection.execute(
-            notif.insert().values(user_id=target.user_id,
-                                message=f'{sender.fullname} have shared you a file, {file.name}',
-                                href= url_for('routes.dashboard'))
-        )
-    except Exception:
-        pass
-
-@event.listens_for(SharedFile, 'before_delete')
-def file_delete(mapper, connection, target):
-    notif = Notification.__table__
-    file = File.query.get(target.file_id)
-    target_notif = Notification.query.filter(Notification.message.contains(file.name)).first()
     try:
         connection.execute(
             notif.delete().where(notif.c.id == target_notif.id)
